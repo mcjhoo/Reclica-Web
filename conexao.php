ff41641f48ac0b62b76c1f450106cd2bc2ade7f0
mcjhoo <jhonatansilva95@hotmail.com>
2018-05-01 00:25:13 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,4 +1,4 @@
 <?php
-    $pdo = new PDO('mysql:dbname=recicla_web;host=localhost', 'root', 'vertrigo', 
+    $pdo = new PDO('mysql:dbname=recicla_web;host=localhost', 'root', '', 
                     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 ?>
